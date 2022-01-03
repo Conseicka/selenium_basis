7fc9e9cdb22db014fd92a5beb36bc84039ca4190
conseicka <>
2022-01-03 15:22:39 -0600

    Hola mundo, en una misma ventana

--- scripts/holaMudo.py
+++ scripts/holaMudo.py
@@ -10,11 +10,11 @@ from selenium import webdriver
 #Test Report: Resumen de resultados
 
 class HelloWorld(unittest.TestCase):
-
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         #Se prepara el entorno de la prueba
-        self.driver = webdriver.Chrome(executable_path='./chromedriver')
-        driver = self.driver
+        cls.driver = webdriver.Chrome(executable_path='./chromedriver')
+        driver = cls.driver
         driver.implicitly_wait(10)
 
     def test_hello_world(self):
@@ -25,9 +25,9 @@ class HelloWorld(unittest.TestCase):
     def test_visit_wikipedia(self):
         #Acciones par finalizar la prueba
         self.driver.get('https://www.wikipedia.org')
-
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name='hello-world-report'))
