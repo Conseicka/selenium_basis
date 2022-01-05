f13992db3c251dc8d86aa72a23df12a2c012f479
conseicka <>
2022-01-05 17:44:36 -0600

    sort table

---
+++ scripts/tables.py
@@ -0,0 +1,43 @@
+import unittest
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DynamicControls(unittest.TestCase):
+
+    def setUp(self):
+        serv = Service('./chromedriver')
+        self.driver = webdriver.Chrome(service = serv)
+        driver = self.driver
+        driver.implicitly_wait(15)
+        driver.maximize_window()
+        driver.get('https://the-internet.herokuapp.com/tables')
+        #driver.find_element(By.LINK_TEXT, 'Typos').click()
+
+
+
+    def test_sort_table(self):
+        driver = self.driver
+
+        table_data = [[] for i in range (5)]
+        print(table_data)
+
+        for i in range(5):
+            header = driver.find_element(By.XPATH, f'//*[@id="table1"]/thead/tr/th[{i+1}]/span')
+            table_data[i].append(header.text)
+
+            for j in range(4):
+                row_data = driver.find_element(By.XPATH, f'//*[@id="table1"]/tbody/tr[{j+1}]/td[{i+1}]')
+                table_data[i].append(row_data.text)
+
+        print(table_data)
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
