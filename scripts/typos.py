ed69bacffdae77f2bf37cbc2d848b55b08d81198
conseicka <>
2022-01-05 16:35:54 -0600

    typos

---
+++ scripts/typos.py
@@ -0,0 +1,49 @@
+import unittest
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DynamicControls(unittest.TestCase):
+
+    def setUp(self):
+        serv = Service('./chromedriver')
+        self.driver = webdriver.Chrome(service = serv)
+        driver = self.driver
+        driver.implicitly_wait(15)
+        driver.maximize_window()
+        driver.get('https://the-internet.herokuapp.com/typos')
+        #driver.find_element(By.LINK_TEXT, 'Typos').click()
+
+    def test_find_typos(self):
+        driver = self.driver
+
+        paragraph_to_check = driver.find_element(By.CSS_SELECTOR, "#content > div > p:nth-child(3)")
+        text_to_check = paragraph_to_check.text
+        print(text_to_check)
+
+        tries = 1
+        found = False
+        correct_text = "Sometimes you'll see a typo, other times you won't."
+
+        while text_to_check != correct_text:
+            paragraph_to_check = driver.find_element(By.CSS_SELECTOR, "#content > div > p:nth-child(3)")
+            text_to_check = paragraph_to_check.text
+            driver.refresh()
+        while not found:
+            if text_to_check == correct_text:
+                tries += 1
+                driver.refresh()
+                found = True
+
+        self.assertEqual(found, True)
+        print(f'It took {tries} tries to find the typo')
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
